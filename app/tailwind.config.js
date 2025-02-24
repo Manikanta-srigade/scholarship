74786ea2f4efdd4ed98f544b090cb16ef5d11787
Manikanta-srigade <srigademanikanta.26@gmail.com>
2025-02-25 02:47:14 +0530

    v1 fixed an error

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -72,6 +72,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+plugins: [require("tailwindcss-animate")],
 }
-
